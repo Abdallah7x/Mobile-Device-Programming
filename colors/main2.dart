6ae0607e5e467d5d02f0b557fdd4c9ab138905ce
Abdallah7x <76527450+Abdallah7x@users.noreply.github.com>
2021-11-15 20:18:16 +0200

    Create main2.dart

---
+++ colors/main2.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter TutorialKart',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pink,
+        title: Text('Learn colors'),
+      ),
+      body: ListView(children: <Widget>[
+        Row(
+          children: [
+            Container(
+              width: 100,
+              height: 200,
+              child: CustomPaint(
+                painter: OpenPainter(),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 200,
+              child: CustomPaint(
+                painter: OpenPainter2(),
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Container(
+              width: 100,
+              height: 200,
+              child: CustomPaint(
+                painter: OpenPainter3(),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 200,
+              child: CustomPaint(
+                painter: OpenPainter4(),
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Container(
+              width: 100,
+              height: 200,
+              child: CustomPaint(
+                painter: OpenPainter5(),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 200,
+              child: CustomPaint(
+                painter: OpenPainter6(),
+              ),
+            ),
+          ],
+        ),
+      ]),
+    );
+  }
+}
+
+class OpenPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint1 = Paint()
+      ..color = Colors.black
+      ..style = PaintingStyle.fill;
+    canvas.drawRect(Offset(10, 30) & const Size(160, 150), paint1);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
+
+class OpenPainter2 extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint1 = Paint()
+      ..color = Colors.red
+      ..style = PaintingStyle.fill;
+    canvas.drawRect(Offset(90, 30) & const Size(160, 150), paint1);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
+
+class OpenPainter3 extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint1 = Paint()
+      ..color = Colors.yellow
+      ..style = PaintingStyle.fill;
+    canvas.drawRect(Offset(190, 10) & const Size(160, 150), paint1);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
+
+class OpenPainter4 extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint1 = Paint()
+      ..color = Colors.purple
+      ..style = PaintingStyle.fill;
+    canvas.drawRect(Offset(-90, 10) & const Size(160, 150), paint1);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
+
+class OpenPainter5 extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint1 = Paint()
+      ..color = Colors.orange
+      ..style = PaintingStyle.fill;
+    canvas.drawRect(Offset(10, 0) & const Size(160, 150), paint1);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
+
+class OpenPainter6 extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint1 = Paint()
+      ..color = Colors.blue
+      ..style = PaintingStyle.fill;
+    canvas.drawRect(Offset(90, 0) & const Size(160, 150), paint1);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
